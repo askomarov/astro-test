b38932a61d402e7b5893a9fe656bedfa803322e1
askomarov <askomarov13@mail.ru>
2023-09-07 13:20:21 +0200

    build: upgrade to astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-import { defineConfig, squooshImageService  } from 'astro/config';
+import { defineConfig, squooshImageService } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  experimental: { assets: true },
   image: {
     remotePatterns: [{ protocol: "https://picsum.photos/" }],
     service: squooshImageService(),
-  }
+  },
 });
